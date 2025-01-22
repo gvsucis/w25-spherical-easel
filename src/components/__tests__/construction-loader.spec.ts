9f3389be1a055a30973cff0dafa5a1210b371fdb
Ronan Kelley <ronankelley2@gmail.com>
2025-01-22 09:59:41 -0500

    annotated and cleaned-up unit test file

--- src/components/__tests__/construction-loader.spec.ts
+++ src/components/__tests__/construction-loader.spec.ts
@@ -7,13 +7,8 @@ import { SphericalConstruction } from "@/types";
 import { Matrix4 } from "three";
 import { useAccountStore } from "@/stores/account";
 
-// vi.mock("vue-i18n", async orig => {
-//   const z = (await orig()) as object;
-//   console.debug("i18N mock", z);
-//   // console.debug("What is this?", orig, "and also this", z);
-//   return { ...z, t: (key: string) => key };
-// });
-
+// generate sample data for the unit tests; produces 3 SphericalConstruction objects
+// in an array with consecutive increasing IDs
 const sampleData = () => {
   const arr: Array<SphericalConstruction> = [];
   for (let k = 0; k < 3; k++) {
@@ -35,14 +30,25 @@ const sampleData = () => {
   return arr;
 };
 
+// mock the firestore class
 vi.mock("firebase/firestore", async fnArg => {
+  // not sure I understand this cast - I think this, combined with the "...firestoreObject" line,
+  // results in a regular firestore object with just the getFirestore/collection/getDocs functions overridden?
   const firestoreObject = (await fnArg()) as object;
 
   // console.debug("Construction-loader.spec.ts: What is this?", firestoreObject);
   return {
+    // "..." is the spread operator, which works almost identically to the
+    // * operator in python.
     ...firestoreObject,
+    // replace the getFirestore method with a mocked version that will return a reference
+    // to the firestore object when called
     getFirestore: vi.fn().mockReturnThis(),
+    // replace the collection method with a mocked version; since no function is given,
+    // this will return `undefined` when invoked.
     collection: vi.fn(),
+    // replace the getDocs method with a mocked version that will execute the lambda passed
+    // to the mockImplementation() call when run
     getDocs: vi.fn().mockImplementation(() => {
       console.debug("Inside mocked getDocs()");
       return {
@@ -52,32 +58,26 @@ vi.mock("firebase/firestore", async fnArg => {
   };
 });
 
-// vi.mock("firebase/storage", async orig => {
-//   const z = (await orig()) as object;
-//   // console.debug("What is this?", z);
-//   return {
-//     ...z,
-//     getStorage: vi.fn().mockReturnThis()
-//   };
-// });
+// mock the firebase auth class; like the firebase class above,
+// I believe this creates an otherwise unmodified firebase auth
+// class but with mocked getAuth and onAuthStateChanged methods.
 vi.mock("firebase/auth", async orig => {
   const z = (await orig()) as object;
-  // console.debug("What is this?", z);
+
   return {
     ...z,
     getAuth: vi.fn().mockReturnThis(),
     onAuthStateChanged: vi.fn()
   };
 });
-// // vi.mock("./firebase", () => mockSDK);
-// config.global.mocks = {
-//   // t: (key: string) => key,
-//   // getAuth: vi.fn()
-// };
 
+// define the actual test environment! Syntax appears very similar to
+// the rake library/DSL for Ruby. This call to "describe" creates a test suite
+// called "construction loader"
 describe("Construction Loader", () => {
   const i18n = createI18n({});
 
+  // reset all mocked symbols before running each individual test
   beforeEach(() => {
     vi.clearAllMocks();
     // vi.stubGlobal("getAuth", vi.fn());
@@ -85,42 +85,51 @@ describe("Construction Loader", () => {
     // vi.fn().mockImplementation(getAuth);
   });
 
+  // define a test named "is a component"; in the suite execution,
+  // this shows up as "Construction Loader > is a component"
   it("is a component", () => {
-    const { wrapper } = createWrapper(TestedComponent, {
-      // globalOptions: {
-      //   stubs: {
-      //     ConstructionList: true
-      //   }
-      // }
-    });
-    // const wrapper = createWrapper(TestedComponent, {
-    //   mountOptions: {
-    //     propsData: {
-    //       items: []
-    //     }
-    //   },
-    //   mockOptions: {
-    //     $appAuth: fakeFirebase.auth(),
-    //     $appDB: fakeFirebase.firestore()
-    //   }
-    // });
+    const { wrapper } = createWrapper(TestedComponent, {});
+    // this call checks if the wrapper object is truthy, which in javascript
+    // means that the wrapper object is not false, null, undefined, NaN, 0, -0, 0n,
+    // "", or document.all.
     expect(wrapper).toBeTruthy();
   });
 
   it("shows public constructions", async () => {
+    // mock all calls to timers (setTimeout, setInterval, clearTimeout, clearInterval,
+    // setImmediate, clearImmediate, and Date)
     vi.useFakeTimers();
+    // createWrapper is a helper function defined in `tests/vue-helper.ts` that creates a
+    // mocked pinia object and a "wrapper" object that is created by a call to
+    // vue's `mount` function, which I don't yet understand.
     const { wrapper, testPinia } = createWrapper(TestedComponent, {});
+    // get a store for the constructions from the mocked pinia object
     const constructionStore = useConstructionStore(testPinia);
+    // create a reference to the sample data we created above
     const testData = sampleData();
+    // place the sample data into the construction store as the public constructions
     constructionStore.publicConstructions = testData;
+    // as I understand it, Vue maintains a virtual DOM that it modifies in place
+    // of the real DOM before syncing the two on a timer. This function call just makes
+    // the sync happen right away instead of waiting, since with timers mocked the wait
+    // would be indefinite. This makes the overall test more predictable and is a good
+    // thing for a unit test to be doing.
     await wrapper.vm.$nextTick();
+    // grab the public panel component
     const pubPanel = wrapper.find("[data-testid=publicPanel");
+    // print the panel's text to the console
     console.debug("Public panel", pubPanel.text());
     // Give the idle timer a chance to run
     await vi.advanceTimersByTime(1500);
+    // print the panel's text to console again; not sure what the "after click" means here.
     console.debug("Public panel (after click)", pubPanel.text());
+    // grab the public list component
     const pub = wrapper.find("[data-testid=publicList");
+    // grab the text of the public list
     const pubText = pub.text();
+    // ensure the public list text contains the author, description, and date created of each
+    // test data element; this will check in the same order that we store them in the test data
+    // array
     testData.forEach(s => {
       expect(pubText).toContain(s.author);
       expect(pubText).toContain(s.description);
@@ -128,11 +137,13 @@ describe("Construction Loader", () => {
     });
   });
 
+  // same as the last test, but with private constructions
   it("shows private constructions", async () => {
     vi.useFakeTimers();
     const { wrapper, testPinia } = createWrapper(TestedComponent, {});
     const constructionStore = useConstructionStore(testPinia);
     const acctStore = useAccountStore(testPinia);
+    // hard coded firebase uids?
     acctStore.firebaseUid = "AbCD17618U";
     await wrapper.vm.$nextTick();
     const testData = sampleData();
@@ -151,6 +162,7 @@ describe("Construction Loader", () => {
     });
   });
 
+  // same as the first 2 tests, but with starred constructions
   it("shows starred constructions", async () => {
     vi.useFakeTimers();
     const { wrapper, testPinia } = createWrapper(TestedComponent, {});
@@ -173,6 +185,7 @@ describe("Construction Loader", () => {
     });
   });
 
+  // test the search functionality; does it filter out non-matching components?
   it("shows filtered list of public constructions", async () => {
     vi.useFakeTimers();
     const { wrapper, testPinia } = createWrapper(TestedComponent, {});
@@ -194,6 +207,7 @@ describe("Construction Loader", () => {
     });
     // console.debug("Public list", publicList.html())
   });
+
   it("shows filtered list of private constructions", async () => {
     vi.useFakeTimers();
     const { wrapper, testPinia } = createWrapper(TestedComponent, {});
