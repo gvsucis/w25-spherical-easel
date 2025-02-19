5a6c15811c9383634703b814c53b8998e5cc1461
Ronan Kelley <44382880+Ronan-Kelley@users.noreply.github.com>
2025-02-19 15:29:06 -0500

    add TODO

--- src/stores/construction.ts
+++ src/stores/construction.ts
@@ -571,6 +571,8 @@ export const useConstructionStore = defineStore("construction", () => {
    *
    * @param targetArr array to fill with the publically visible constructions
    * @return nothing - the passed array is directly modified.
+   *
+   * TODO look into making sure this function executed as expected
    */
   async function parsePublicCollection(
     targetArr: Array<SphericalConstruction>
